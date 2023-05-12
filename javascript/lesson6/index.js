77ac805836cd57804e244fdf61fc403d81aa8cac
Cristiano Sampaio <cristiano@mindsight.com.br>
2023-05-12 10:22:12 -0300

    add lesson 6

---
+++ javascript/lesson6/index.js
@@ -0,0 +1,14 @@
+// Strings
+const name = 'Cris';
+const name1 = "Cris";
+const name2 = `Cris`;
+
+// Number
+const num1 = 10;
+const num2 = 3.14; 
+
+// Undefined
+let variable;
+
+// Bool
+const boolean = true;

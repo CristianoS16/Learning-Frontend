7b2dcc6864f6c5a6b1fd8212d65ac1a36306952d
Cristiano Sampaio <cristiano@mindsight.com.br>
2023-05-12 10:46:52 -0300

    add lesson 7

---
+++ javascript/lesson7/index.js
@@ -0,0 +1,21 @@
+/*
+()
+**
+* / %
++ - 
+*/
+
+// Concat
+const strNum = '1';
+const num  = 10;
+console.log(strNum+num);
+console.log(num+strNum);
+
+
+// NaN -> not a number
+
+//String to Number
+const strFloat = '1.5'
+console.log(typeof parseInt(strFloat))
+console.log(typeof parseFloat(strFloat))
+console.log(typeof Number(strFloat))

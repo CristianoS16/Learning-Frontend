c06dc5d4af64f7f47622a1076a00b869a712bfff
Cristiano <cristianosampaio16@gmail.com>
2023-12-17 19:45:37 -0300

    chore: improve base with new strings

--- regex/class1.js
+++ regex/class1.js
@@ -2,12 +2,11 @@
 // i -> insensitive case for words
 // () --> create groups
 
-const { text } = require("./base");
+const { texto } = require("./base");
 
-const regex1 = /(adipisicing) (elit.)/i;
-const found = regex1.exec(text);
-if (found) {
+const regex1 = /(maria|ana)(, hoje sua esposa)/i;
+const found = regex1.exec(texto);
   console.log(found[0]);
   console.log(found[1]);
   console.log(found[2]);
-}
+
